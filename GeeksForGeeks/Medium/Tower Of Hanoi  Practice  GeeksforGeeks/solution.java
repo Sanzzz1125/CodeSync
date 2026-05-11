875eb4f76f09b63ac4a7df0e6644a6dcfbb48cfa
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 23:21:52 +0530

    Solved Tower Of Hanoi | Practice | GeeksforGeeks

--- GeeksForGeeks/Medium/Tower Of Hanoi  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Medium/Tower Of Hanoi  Practice  GeeksforGeeks/solution.java
@@ -1,6 +1,5 @@
 class Solution {
     public int towerOfHanoi(int n, int from, int to, int aux) {
-        // code here
-        return (int)Math.pow(2,n)-1;
+            return (int)Math.pow(2,n)-1;
     }
 }

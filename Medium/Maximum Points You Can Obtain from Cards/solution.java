f9a37bb7c53739ac7b047a3388e6616374d8da12
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 12:11:43 +0530

    Solved Maximum Points You Can Obtain from Cards

--- Medium/Maximum Points You Can Obtain from Cards/solution.java
+++ Medium/Maximum Points You Can Obtain from Cards/solution.java
@@ -1,10 +1,18 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int sum=0;
-        for(int i = cardPoints.length-1 ;i>=0 && k!=0;i--){
+        int n = cardPoints.length;
+        int sum=0,left=0,max=Integer.MIN_VALUE;
+        for(int i=0;i<k;i++){
             sum += cardPoints[i];
-            k--;
         }
-        return sum;
+        max = Math.max(sum,max);
+        int i=k-1,j=n-1;
+        while(i>=0){
+            sum = sum + cardPoints[j] - cardPoints[i];
+            i--;
+            j--;
+            max = Math.max(max,sum);
+        }
+        return max;
     }
 }

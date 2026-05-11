9e672a17a0d649b345a6a934e7060934b55531fc
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 10:26:18 +0530

    Solved Maximum Number of Vowels in a Substring of Given Length

--- Medium/Maximum Number of Vowels in a Substring of Given Length/solution.java
+++ Medium/Maximum Number of Vowels in a Substring of Given Length/solution.java
@@ -1,14 +1,41 @@
 class Solution {
+    public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
     public int maxVowels(String s, int k) {
-        int cnt=Integer.MIN_VALUE;
-        for(int i=0;i<=s.length()-k;i++){
-            int temp=0;
-            for(int j=i;j<i+k;j++){
-                char ch = s.charAt(j);
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') temp++;
+        int n = s.length();
+
+        int cnt=0;
+        int maxCnt = cnt;
+
+        for(int i=0;i<n;i++){
+            if(isVowel(s.charAt(i))){
+                cnt++;
             }
-            cnt = Math.max(cnt,temp);
+
+            if(i >= k && isVowel(s.charAt(i-k))){
+                cnt--;
+            }
+
+            if(i >= k-1){
+                maxCnt = Math.max(maxCnt,cnt);
+            }
         }
-        return cnt;
+        return maxCnt;
     }
 }
+
+// class Solution {
+//     public int maxVowels(String s, int k) {
+//         int cnt=Integer.MIN_VALUE;
+//         for(int i=0;i<=s.length()-k;i++){
+//             int temp=0;
+//             for(int j=i;j<i+k;j++){
+//                 char ch = s.charAt(j);
+//                 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') temp++;
+//             }
+//             cnt = Math.max(cnt,temp);
+//         }
+//         return cnt;
+//     }
+// }

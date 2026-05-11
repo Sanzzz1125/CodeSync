2d0e67168be86a238367eba4e45b1df0e5852a8d
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 22:00:30 +0530

    Solved Separate the Digits in an Array

---
+++ LeetCode/Easy/Separate the Digits in an Array/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        int n = nums.length;
+
+        for(int i=0;i<nums.length;i++){
+            int temp = nums[i];
+            int cnt=0;
+            while(temp!=0){
+                cnt++;
+                temp = temp/10;
+            }
+            n += cnt-1;
+        }
+
+        int arr[] = new int[n];
+        n=n-1;
+
+        for(int i=nums.length-1;i>=0;i--){
+            int temp = nums[i];
+            int cnt=0;
+            while(temp!=0){
+                arr[n--] = temp%10;
+                temp /= 10;
+            }
+        }
+        
+        return arr;
+    }
+}

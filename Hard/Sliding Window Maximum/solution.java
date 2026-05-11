b92d4f7a7a322e7e0e5282ecb05c0ae39b2f837c
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 16:09:05 +0530

    Solved Sliding Window Maximum

--- Hard/Sliding Window Maximum/solution.java
+++ Hard/Sliding Window Maximum/solution.java
@@ -1,28 +1,24 @@
 class Solution {
 
+    public int findMax(int[] nums, int start, int end) {
+        int max = Integer.MIN_VALUE;
+        for(int i = start; i <= end; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-
         int n = nums.length;
-
         List<Integer> list = new ArrayList<>();
-
         for(int i = 0; i <= n-k; i++) {
-
-            int max = Integer.MIN_VALUE;
-
-            for(int j = i; j < i+k; j++) {
-                max = Math.max(max, nums[j]);
-            }
-
+            int max = findMax(nums, i, i+k-1);
             list.add(max);
         }
-
         int[] ans = new int[list.size()];
-
         for(int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
         }
-
         return ans;
     }
 }

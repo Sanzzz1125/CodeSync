06d5cad98e14b3dfb75e2348242acad6bdf97350
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 15:27:14 +0530

    Solved Grumpy Bookstore Owner

--- Medium/Grumpy Bookstore Owner/solution.java
+++ Medium/Grumpy Bookstore Owner/solution.java
@@ -7,7 +7,6 @@ class Solution {
         }
 
         int newSum = 0;
-
         for(int i=0;i<minutes;i++){
             if(grumpy[i] == 1){
                 newSum += customers[i];
@@ -15,14 +14,12 @@ class Solution {
         }
 
         int max = newSum;
-
         for(int i=minutes;i<n;i++){
             if(grumpy[i] == 1) newSum += customers[i];
 
             if(grumpy[i - minutes] == 1) newSum -= customers[i-minutes];
 
             max = Math.max(newSum,max);
-
         }
         return max+already;
     }

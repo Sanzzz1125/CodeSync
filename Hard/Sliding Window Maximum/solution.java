513e913bfd04cb1bf398866727ae1c406b078c8f
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 16:47:39 +0530

    Solved Sliding Window Maximum

--- Hard/Sliding Window Maximum/solution.java
+++ Hard/Sliding Window Maximum/solution.java
@@ -1,24 +1,22 @@
 class Solution {
-
-    public int findMax(int[] nums, int start, int end) {
-        int max = Integer.MIN_VALUE;
-        for(int i = start; i <= end; i++) {
-            max = Math.max(max, nums[i]);
-        }
-        return max;
-    }
-
     public int[] maxSlidingWindow(int[] nums, int k) {
+        
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i <= n-k; i++) {
-            int max = findMax(nums, i, i+k-1);
-            list.add(max);
+        int[] result = new int[n - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        for(int i=0;i<n;i++){
+            while(!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
+                deque.pollLast();
+            }
+            deque.add(i);
+            if(!deque.isEmpty() && i - k >= deque.peekFirst()){
+                deque.pollFirst();
+            }
+            if(i >= k-1){
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
         }
-        int[] ans = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i);
-        }
-        return ans;
+        return result;
     }
 }

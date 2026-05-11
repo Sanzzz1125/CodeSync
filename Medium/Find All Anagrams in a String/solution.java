f51e58e4a24702d14a61db0d3a6fdec41fc410d5
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-11 12:18:12 +0530

    Solved Find All Anagrams in a String

--- Medium/Find All Anagrams in a String/solution.java
+++ Medium/Find All Anagrams in a String/solution.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        char[] arr = p.toCharArray();
-        Arrays.sort(arr);
-        p = new String(arr);
-        int k = p.length();
-        for(int i=0;i<s.length();i++){
-            if(i>=k-1){
-                char[] temp = s.substring(i-(k-1),i+1).toCharArray();
-                Arrays.sort(temp);
-                String sorted = new String(temp);
-                if(sorted.equals(p)){
-                    list.add(i-(k-1));
-                }
+        int[] arr1=new int[26];
+        int[] arr2=new int[26];
+        List<Integer> ans=new ArrayList<Integer>();
+        if(p.length()>s.length()){
+            return ans;
+        }
+        for(char ch : p.toCharArray()){
+            arr2[ch-'a']+=1;
+        }
+        for(int i=0;i<p.length();i++){
+            arr1[s.charAt(i)-'a']+=1;
+        }
+        if(Arrays.equals(arr1,arr2)){
+            ans.add(0);
+        }
+        for(int i=p.length();i<s.length();i++){
+            arr1[s.charAt(i)-'a']+=1;
+            arr1[s.charAt(i-p.length())-'a']-=1;
+        
+            if(Arrays.equals(arr1,arr2)){
+                ans.add(i-p.length()+1);
             }
         }
-        return list;
+        return ans;
     }
 }

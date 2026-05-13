906205d26c337654d667543aa53c0a9716ee1926
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-13 16:10:34 +0530

    Solved Get Min from Stack | Practice | GeeksforGeeks

---
+++ GeeksForGeeks/Medium/Get Min from Stack  Practice  GeeksforGeeks/solution.java
@@ -0,0 +1,53 @@
+class SpecialStack {
+
+    static class Node {
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min) {
+            this.val = val;
+            this.min = min;
+        }
+    }
+
+    Node head;
+
+    public SpecialStack() {
+        head = null;
+    }
+
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x);
+        } else {
+            Node curr = new Node(x, Math.min(x, head.min));
+            curr.next = head;
+            head = curr;
+        }
+    }
+
+    public void pop() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+    public int peek() {
+        if (head == null) {
+            return -1;
+        }
+        return head.val;
+    }
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    public int getMin() {
+        if (head == null) {
+            return -1;
+        }
+        return head.min;
+    }
+}

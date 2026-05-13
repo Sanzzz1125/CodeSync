f6e1daac4707282086775b29d648cbb6d54bda29
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-13 16:52:36 +0530

    Solved Ways To Tile A Floor | Practice | GeeksforGeeks

--- GeeksForGeeks/Medium/Ways To Tile A Floor  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Medium/Ways To Tile A Floor  Practice  GeeksforGeeks/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int numberOfWays(int n) {
-        // code here
+        
         int a = 0, b = 1;
         for (int i = 2; i <= n + 1; i++) {
             int c = a + b;
             a = b;
             b = c;
         }
+        
         return b;
     }
 };

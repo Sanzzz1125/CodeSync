f215a5945341796fb9f36307777de386d0f3eb67
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-13 12:23:37 +0530

    Solved Longest Substring with K Uniques | Practice | GeeksforGeeks

---
+++ GeeksForGeeks/Medium/Longest Substring with K Uniques  Practice  GeeksforGeeks/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestKSubstr(String s, int k) {
+        // code here
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        int maxLen = -1, left = 0;
+        
+        for(int right = 0;right<s.length(); right++){
+            char ch = s.charAt(right);
+            
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            
+            while(map.size() > k){
+                char lChar = s.charAt(left);
+                
+                map.put(lChar, map.get(lChar)-1);
+                
+                if(map.get(lChar) == 0) map.remove(lChar);
+                
+                left++;
+            }
+            
+            if(map.size() == k){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+    }
+}

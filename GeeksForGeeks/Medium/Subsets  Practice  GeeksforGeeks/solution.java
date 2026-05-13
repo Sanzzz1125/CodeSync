a8e987bf287b55061805c6df97465340206540e9
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 00:28:57 +0530

    Solved Subsets | Practice | GeeksforGeeks

--- GeeksForGeeks/Medium/Subsets  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Medium/Subsets  Practice  GeeksforGeeks/solution.java
@@ -4,6 +4,7 @@ class Solution {
         Arrays.sort(arr);
         
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        
         helper(arr, 0, new ArrayList<>(), res);
         
         return res;
@@ -11,16 +12,14 @@ class Solution {
 
     private void helper(int[] arr, int index,ArrayList<Integer> curr,ArrayList<ArrayList<Integer>> res) {
         if (index == arr.length) {
-            
             res.add(new ArrayList<>(curr));
             return;
         }
-        curr.add(arr[index]);
         
+        curr.add(arr[index]);
         helper(arr, index + 1, curr, res);
         
         curr.remove(curr.size() - 1);
-        
         helper(arr, index + 1, curr, res);
     }
 }

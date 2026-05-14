2a6df0427d13ead977ebb272ddd9e74c11789065
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 10:20:08 +0530

    Solved Baseball Game

--- LeetCode/Easy/Baseball Game/solution.java
+++ LeetCode/Easy/Baseball Game/solution.java
@@ -3,7 +3,24 @@ class Solution {
         Stack<Integer> stk = new Stack<>();
         int len = operations.length;
         for(int i = 0;i<len;i++){
-            
+            if(operations[i].equals("C")){
+                stk.pop();
+            }else if(operations[i].equals("D")){
+                stk.push(stk.peek()*2);
+            }else if(operations[i].equals("+")){
+                int n1 = stk.pop();
+                int n2 = stk.pop();
+                stk.push(n2);
+                stk.push(n1);
+                stk.push(n1+n2);
+            }else{
+                stk.push(Integer.parseInt(operations[i]));
+            }
         }
+        int sum = 0;
+        for (int x : stk) {
+            sum += x;
+        }
+        return sum;
     }
 }

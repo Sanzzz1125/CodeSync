e2c777a461ec91e7788f4051ca510eaab60f3af1
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 10:12:31 +0530

    Solved Baseball Game

--- LeetCode/Easy/Baseball Game/solution.java
+++ LeetCode/Easy/Baseball Game/solution.java
@@ -1,29 +1,9 @@
 class Solution {
     public int calPoints(String[] operations) {
-        int l = operations.length;
-        Stack<Integer> s = new Stack<>();
-        for(int i=0;i<l;i++){
-            if(operations[i].equals("C")){
-                s.pop();
-            }
-            else if(operations[i].equals("D")){
-                int num = s.peek();
-                s.push(num*2);
-            }
-            else if(operations[i].equals("+")){
-                int num1 = s.pop();
-                int num2 = s.pop();
-                s.push(num2);
-                s.push(num1);
-                s.push(num1+num2);
-            }else{
-                s.push(Integer.parseInt(operations[i]));
-            }
+        Stack<Integer> stk = new Stack<>();
+        int len = operations.length;
+        for(int i = 0;i<len;i++){
+            
         }
-        int sum = 0;
-        for (int x : s) {
-            sum += x;
-        }
-        return sum;
     }
 }

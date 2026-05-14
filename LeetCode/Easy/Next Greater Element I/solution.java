af09fc19dc3f7134a701b78eb40478b881ba0d5b
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 11:45:53 +0530

    Solved Next Greater Element I

--- LeetCode/Easy/Next Greater Element I/solution.java
+++ LeetCode/Easy/Next Greater Element I/solution.java
@@ -2,8 +2,10 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
+
         int[] arr = new int[n1];
         int k=0;
+
         for(int i=0;i<n1;i++){
             arr[i] = -1;
             for(int j=0;j<n2;j++){
@@ -18,6 +20,7 @@ class Solution {
                 }
             }
         }
+        
         return arr;
     }
 }

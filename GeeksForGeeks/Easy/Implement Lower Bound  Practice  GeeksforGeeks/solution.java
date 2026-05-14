61621e6af6cea85e5624e850348cd60bf8569a3b
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 09:35:05 +0530

    Solved Implement Lower Bound | Practice | GeeksforGeeks

---
+++ GeeksForGeeks/Easy/Implement Lower Bound  Practice  GeeksforGeeks/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int l=0,h=arr.length-1;
+            int ans=arr.length;
+        
+        while(l<=h){
+            int m = l+(h-l)/2;
+            if(arr[m]>=target){
+                ans = m;
+                h=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        
+        return ans;
+    }
+}

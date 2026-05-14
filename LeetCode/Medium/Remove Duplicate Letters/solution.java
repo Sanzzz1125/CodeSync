0d982401e89eb4df7ec3e2ddfb8029e52fd9badb
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-14 21:17:53 +0530

    Solved Remove Duplicate Letters

--- LeetCode/Medium/Remove Duplicate Letters/solution.java
+++ LeetCode/Medium/Remove Duplicate Letters/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         int[] freq = new int[26];
-
         for(char ch : s.toCharArray()){
             freq[ch - 'a']++;
         }
@@ -26,7 +25,6 @@ class Solution {
         for (char ch : stk) {
             sb.append(ch);
         }
-
         return sb.toString();
     }
 }

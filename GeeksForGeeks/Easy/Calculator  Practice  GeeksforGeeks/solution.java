cc1b9e093328638239aed031f0638075e05cad8a
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-15 11:42:12 +0530

    Solved Calculator | Practice | GeeksforGeeks

---
+++ GeeksForGeeks/Easy/Calculator  Practice  GeeksforGeeks/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void utility(int a, int b, int opr) {
+        // code here
+        if(opr == 1) System.out.print(a+b);
+        else if(opr == 2) System.out.print(a-b);
+        else if(opr == 3) System.out.print(a*b);
+        else System.out.print("Invalid Input");
+    }
+}

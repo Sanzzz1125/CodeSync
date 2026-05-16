2c0c668caefc4af106e50da715a97f617e1e8c10
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-16 12:28:44 +0530

    Solved Sorted and Rotated Minimum | Practice | GeeksforGeeks

--- GeeksForGeeks/Easy/Sorted and Rotated Minimum  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Easy/Sorted and Rotated Minimum  Practice  GeeksforGeeks/solution.java
@@ -1,10 +1,8 @@
 class Solution {
     public int findMin(int[] nums) {
-        
         int low = 0, high = nums.length - 1;
 
         while(low < high){
-            
             int mid = low + (high - low)/2;
             
             if(nums[mid] < nums[high]){

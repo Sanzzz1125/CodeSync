0537f31fbb4921651bb34b8fdb04da0549671a35
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-16 14:43:15 +0530

    Solved Path Sum III

---
+++ LeetCode/Medium/Path Sum III/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+
+        return countPath(root, targetSum)
+                + pathSum(root.left, targetSum)
+                + pathSum(root.right, targetSum);
+    }
+
+    private int countPath(TreeNode node, long targetSum) {
+        if (node == null) return 0;
+
+        int count = 0;
+
+        if (node.val == targetSum)
+            count++;
+
+        count += countPath(node.left, targetSum - node.val);
+        count += countPath(node.right, targetSum - node.val);
+
+        return count;
+    }
+}

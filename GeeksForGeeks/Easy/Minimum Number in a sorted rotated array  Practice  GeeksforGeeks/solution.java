48fd7fff7fb1d19fc07c7b99809396a142ffbf67
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-16 14:12:45 +0530

    Solved Minimum Number in a sorted rotated array | Practice | GeeksforGeeks

--- GeeksForGeeks/Easy/Minimum Number in a sorted rotated array  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Easy/Minimum Number in a sorted rotated array  Practice  GeeksforGeeks/solution.java
@@ -1,13 +1,13 @@
 class Solution {
-    // Function to find the minimum element in sorted and rotated array.
     static int minNumber(int arr[], int low, int high) {
-        // Your code here
+        
        low = 0;
        high = arr.length - 1;
 
         while (low < high) {
+            
             int mid = low + (high - low) / 2;
-
+    
             if (arr[mid] < arr[high]) {
                 high = mid;
             } 
@@ -18,7 +18,7 @@ class Solution {
                 high--;
             }
         }
-
+        
         return arr[low];
     }
 }

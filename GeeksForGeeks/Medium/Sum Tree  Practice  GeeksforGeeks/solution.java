16c69202123fc534edc2b184fa70304e1d0e1ce0
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-16 12:37:24 +0530

    Solved Sum Tree | Practice | GeeksforGeeks

--- GeeksForGeeks/Medium/Sum Tree  Practice  GeeksforGeeks/solution.java
+++ GeeksForGeeks/Medium/Sum Tree  Practice  GeeksforGeeks/solution.java
@@ -14,15 +14,29 @@ class Node
 class Solution {
     int sum(Node root){
         if(root==null) return 0;
-        if(root.left==null && root.right==null) return root.data;
+        
+        if(root.left==null && root.right==null){
+            return root.data;
+        }
         int left=sum(root.left);
-        if(left==-1) return -1;
+        
+        if(left==-1){
+            return -1;
+        }
         int right=sum(root.right);
-        if(right==-1) return -1;
-        if(root.data!=left+right) return -1;
+        
+        if(right==-1){
+            return -1;
+        }
+        
+        if(root.data!=left+right){
+            return -1;
+        }
+        
         return root.data+left+right;
     }
     boolean isSumTree(Node root) {
+        
         return sum(root)!=-1;
     }
 }

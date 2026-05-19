2566137b8e3319a78464c8eabeea440e28616985
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-19 10:02:26 +0530

    Solved Kth Smallest Element in a BST

--- LeetCode/Medium/Kth Smallest Element in a BST/solution.java
+++ LeetCode/Medium/Kth Smallest Element in a BST/solution.java
@@ -13,19 +13,41 @@
  *     }
  * }
  */
+
 class Solution {
-    List<Integer> list = new ArrayList<>();
+    int res = 0, i=0;
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root);
-        return list.get(k-1);
+        inorder(root,k);
+        return res;
     }
 
-    public void inorder(TreeNode root){
+    public void inorder(TreeNode root, int k){
         if(root == null){
             return;
         }
-        inorder(root.left);
-        list.add(root.val);
-        inorder(root.right);
+        inorder(root.left,k);
+        i++;
+        if(i == k){
+            res = root.val;
+        }
+        inorder(root.right,k);
     }
 }
+
+
+// class Solution {
+//     List<Integer> list = new ArrayList<>();
+//     public int kthSmallest(TreeNode root, int k) {
+//         inorder(root);
+//         return list.get(k-1);
+//     }
+
+//     public void inorder(TreeNode root){
+//         if(root == null){
+//             return;
+//         }
+//         inorder(root.left);
+//         list.add(root.val);
+//         inorder(root.right);
+//     }
+// }

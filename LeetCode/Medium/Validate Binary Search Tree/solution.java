212f672ad956c4c69e3afc33f97478cb33771717
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-05-19 11:20:59 +0530

    Solved Validate Binary Search Tree

--- LeetCode/Medium/Validate Binary Search Tree/solution.java
+++ LeetCode/Medium/Validate Binary Search Tree/solution.java
@@ -15,21 +15,17 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null)
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean validate(TreeNode node, long min, long max){
+        if(node == null){
             return true;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode pre = null;
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            if (pre != null && root.val <= pre.val)
-                return false;
-            pre = root;
-            root = root.right;
         }
-        return true;
+
+        if(node.val <= min || node.val >= max){
+            return false;
+        }
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
     }
 }

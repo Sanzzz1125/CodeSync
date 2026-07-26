71ae6d1fb98402d2aaa7c4f77621ab433a605dae
Sanketh Thatikonda <sanketh.thatikonda@gmail.com>
2026-07-26 17:04:54 +0530

    Solved Maximum Product of Three Numbers

--- LeetCode/Easy/0628-Maximum Product of Three Numbers/solution.java
+++ LeetCode/Easy/0628-Maximum Product of Three Numbers/solution.java
@@ -3,7 +3,8 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
-        int max = nums[n-1] * nums[n-2] * nums[n-3];
-        return max;
+        int max1 = nums[n-1] * nums[n-2] * nums[n-3];
+        int max2 = nums[0] * nums[1] * nums[2];
+        return max1>max2 ? max1 : max2;
     }
 }
